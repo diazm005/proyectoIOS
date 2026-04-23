297a0b5b6ad173e8c0f919f47382ffacf1094861
diazm005 <diazmio2005@gmail.com>
2026-04-23 10:34:31 -0400

    Avance pantallas principales iOS

---
+++ proyectoFinal/proyectoFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proyectoFinal
+//
+//  Created by user290070 on 4/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
